3ea2ddf59fd5864488f16beff427387ac56320e1
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2015-06-25 16:04:15 +0500

    Adding moment.js via main_requirejs.js
    TNL-2413

---
+++ common/lib/xmodule/xmodule/js/spec/main_requirejs.js
@@ -0,0 +1,8 @@
+(function(requirejs, define) {
+    requirejs.config({
+        paths: {
+            'moment': 'common_static/js/vendor/moment.min'
+        }
+    });
+
+}).call(this, RequireJS.requirejs, define);
