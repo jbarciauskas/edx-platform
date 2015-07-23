5df9280e223d737f09d9e3c94fee347eba872a59
Jesse Zoldak <zoldak@edx.org>
2015-07-23 15:46:22 -0400

    Go back to yml and sh files

---
+++ scripts/circle.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+case $CIRCLE_NODE_INDEX in
+    0) paver test_lib -l common/lib/calc ;;
+
+    1) paver test_system -t lms/djangoapps/courseware/tests/tests.py:ActivateLoginTest ;;
+
+    2) paver test_system -t cms/djangoapps/contentstore/tests/tests.py:AuthTestCase ;;
+esac
+
+RET=$?
+
+cp -r reports/. $CIRCLE_TEST_REPORTS
+
+exit $RET
