222c42db7a96b9bd357641d8c0cf558776de012c
Jesse Zoldak <zoldak@edx.org>
2015-07-23 12:28:15 -0400

    Run example tests at circleci

--- scripts/circle.sh
+++ scripts/circle.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env sh
 
-paver --help
+case $CIRCLE_NODE_INDEX in
+    0) paver test_lib -l common/lib/xmodule ;;
+
+    1) paver test_system -t lms/djangoapps/courseware/tests/tests.py:ActivateLoginTest ;;
+
+    2) paver test_system -t cms/djangoapps/contentstore/tests/tests.py:AuthTestCase  ;;
+esac
+
+RET=$?
+
+cp -r reports/. $CIRCLE_TEST_REPORTS
+
+exit $RET
