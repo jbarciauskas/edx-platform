6401c76f4cd48ee6990f3f5477541c6dd28ac82e
Jesse Zoldak <zoldak@edx.org>
2015-07-23 14:02:15 -0400

    Move test logic into circle yml file

--- scripts/circle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-case $CIRCLE_NODE_INDEX in
-    0) paver test_lib -l common/lib/xmodule ;;
-
-    1) paver test_system -t lms/djangoapps/courseware/tests/tests.py:ActivateLoginTest ;;
-
-    2) paver test_system -t cms/djangoapps/contentstore/tests/tests.py:AuthTestCase  ;;
-esac
